dfe9de3fbd30f007bc368d86f967ab0d01c72ead
Ipfi Lidzhade <g16L5376@ict.ru.ac.za>
2019-04-15 12:27:02 +0200

    started working on problem F

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -64,6 +64,62 @@ let commaSprinkler input =
     failwith "Not implemented"
 
 let rivers input =
+    // takes in the input and returns a string list of the words and space characters e.g ["Like"; " "; "this"]
+    let inputList (input:string) =
+        let rec apply (input:string) (newlist:string list) word =
+            match input.[0],input.Length > 1 with
+            | _, false -> word::newlist
+            | ' ', true -> apply input.[1..] (" "::word::newlist) ""
+            | a, true -> apply input.[1..] (newlist) (word + string a)
+        List.rev (apply (input) ([]) (""))
+    
+    //takes in a list of strings and a line width and returns a list with the index of each space character
+    //eg. [5, 8, 13, -1, 4] where -1 represents the start of a new line
+    let spaceIndxList (input:string list) (lineWidth:int) =
+        let rec apply (newlist:int list) (indx:int) (spaceindx:int) (length:int) =
+            match indx < input.Length with
+            | false -> newlist
+            | true -> 
+                match length + input.[indx].Length <= lineWidth with
+                | false ->
+                    match input.[indx] with
+                    | " " -> apply (-1::newlist) (indx + 1) 0 0
+                    | x -> apply (-1::newlist) (indx + 1) (x.Length) (x.Length)
+                | true ->
+                    match input.[indx] with
+                    | " " -> apply (spaceindx::newlist) (indx + 1) (spaceindx + 1) (length + 1)
+                    | x -> apply newlist (indx + 1) (spaceindx + x.Length) (length + x.Length)
+        List.rev (apply [] 0 0 0)
+    
+    (*spaceIndxList ["When"; " "; "two"; " "; "or"; " "; "more"; " "; "rivers"; " "; "meet"; " ";
+   "at"; " "; "a"; " "; "confluence"; " "; "other"; " "; "than"; " "; "the";
+   " "; "sea"; " "; "the"; " "; "resulting"; " "; "merged"; " "; "river"; " ";
+   "takes"; " "; "the"; " "; "name"; " "; "of"; " "; "one"; " "; "of"; " ";
+   "those"; " "; "river"] 10*)
+
+    //returns the longest river found
+    let findRivers spaceIndxList = failwith "Not implemented"
+        (*let rec apply (spaceIndxList:int list) (riverLengths:int list) (length:int)=
+            let count = List.find (x = -1) spaceIndxList
+            let x = 0
+            match x < count with
+            | false -> riverLengths
+            | true ->
+                indx = x
+                let rec loop (indx:int)=
+                    match indx < spaceIndxList.Length with
+                    | false -> length::riverLengths
+                    | true -> 
+                        match spaceIndxList.[indx] with
+                        | -1 -> loop (indx + 1)*)
+                    
+
+    // takes in a space index list and a line width and returns the longest river
+    let riverFinder spaceIndxList lineWidth =
+        failwith "Not implemented"    
+
+    
+
     failwith "Not implemented"
 
 [<EntryPoint>]
